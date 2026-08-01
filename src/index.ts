d42df8e2ba03ee45ea7defb5bd50deb912797230
s1-1985 <raven19850115@hotmail.co.jp>
2026-08-01 14:59:00 +0900

    Export competitor AI simulation APIs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 export { loadScenario } from "./data/loaders/loadScenario.js";
 export { createSimulation } from "./simulation/simulation.js";
 export type { PolicyCommand, Simulation, SimulationSnapshot } from "./simulation/simulation.js";
+export {
+  createCompetitiveSimulation,
+} from "./simulation/competitiveSimulation.js";
+export type {
+  CompetitiveDailyReport,
+  CompetitiveSimulation,
+  CompetitiveSimulationSnapshot,
+} from "./simulation/competitiveSimulation.js";
+export {
+  COMPETITOR_ACTION_IDS,
+  createCompetitorAI,
+} from "./simulation/competitor.js";
+export type {
+  CompetitorActionEvent,
+  CompetitorActionId,
+  CompetitorAI,
+  CompetitorAISnapshot,
+  CompetitorDecisionEvent,
+  CompetitorPerceivedSignals,
+  CompetitorPublicObservation,
+  CompetitorStorePublicState,
+} from "./simulation/competitor.js";
 export type { OperationTaskId, OperationTaskRecord } from "./simulation/operations.js";
 export { createHabitSystem, habitForTimeBlock, HABIT_IDS } from "./simulation/habits.js";
 export type {
